985c42f66f5da042b858934d779b773fb6f1f19b
Dany Ouaras <dany.ouaras.bbo@banctec.fr>
2012-06-15 16:23:19 +0200

    First Commit, Boucle for

---
+++ BoucleFor.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    for(int rey = 1; rey < 5; rey++)
+        cout << rey << endl;
+    
+    system("pause");
+}
